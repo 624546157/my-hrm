f76e31a2d0420ad2c559178f7f1507943a580fa8
anran123 <wuzebin601@163.com>
2019-09-20 14:16:15 +0800

    前端代码

---
+++ project-saas-hrm-vue-master/src/components/Breadcrumb/index.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="app-breadcrumb">
+  山西省垣曲县望仙村村民管理系统<span class="breadBtn">体验版-练手</span>
+</div>
+</template>
+
+<script>
+import {generateTitle} from '@/utils/i18n'
+
+export default {
+  created() {
+    this.getBreadcrumb()
+  },
+  data() {
+    return {
+      levelList: null
+    }
+  },
+  watch: {
+    $route() {
+      this.getBreadcrumb()
+    }
+  },
+  methods: {
+    generateTitle,
+    getBreadcrumb() {
+      let matched = this.$route.matched.filter(item => item.name)
+      const first = matched[0]
+      if (first && first.name !== 'dashboard') {
+        matched = [{path: '/dashboard', meta: {title: 'dashboard'}}].concat(
+          matched
+        )
+      }
+      this.levelList = matched
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" background scoped>
+.app-breadcrumb {
+  display: inline-block;
+  font-size: 18px;
+  line-height: 50px;
+  margin-left: 10px;
+  color: #e6dcdc;
+  cursor: text;
+  .breadBtn{background: #b4b4bd; font-size: 14px ;padding:0 10px;display: inline-block;height: 30px; line-height: 30px;border-radius: 10px; margin-left: 15px;}
+}
+</style>
---
+++ project-saas-hrm-vue-master/src/module-saas-clients/pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-card shadow="never">
+        <!--elementui的table组件
+          data：数据模型
+         -->
+         <!-- 数据 -->
+        <el-table :data="dataList"  fit highlight-current-row style="width: 100%" border>
+          <el-table-column fixed prop="id" label="ID"  width="40" align="center"></el-table-column>
+          <el-table-column fixed prop="name" label="企业名称"  width="150" align="center"></el-table-column>
+          <el-table-column fixed prop="managerId" label="企业登录账号"  width="100" align="center"></el-table-column>
+          <el-table-column fixed prop="renewalDate" label="续期时间"  width="100" align="center"></el-table-column>
+          <el-table-column fixed prop="expirationDate" label="到期时间"  width="100" align="center"></el-table-column>
+          <el-table-column fixed prop="companyAddress" label="公司地址"  width="200" align="center"></el-table-column>
+          <el-table-column fixed prop="companyPhone" label="公司电话"  width="150" align="center"></el-table-column>
+          <el-table-column fixed prop="mailbox" label="邮箱"  width="150" align="center"></el-table-column>
+          <el-table-column fixed prop="industry" label="所属行业"  width="150" align="center"></el-table-column>
+           <el-table-column fixed prop="balance" label="当前余额"  width="150" align="center"></el-table-column>
+          <el-table-column fixed prop="companySize" label="公司规模"  width="50" align="center"></el-table-column>
+          <el-table-column fixed prop="auditState" label="审核状态"  width="50" align="center"></el-table-column>
+          <el-table-column fixed prop="state" label="状态"  width="50" align="center"></el-table-column>         
+          <el-table-column fixed prop="remarks" label="备注"  width="200" align="center"></el-table-column>
+          <el-table-column fixed prop="createTime" label="创建时间"  width="150" align="center"></el-table-column>
+		  
+		      <el-table-column fixed="right" label="操作" width="150">
+              <template slot-scope="scope">
+                <router-link :to="'/saas-clients/details/'+scope.row.id">查看</router-link>
+              </template>
+          </el-table-column>
+        </el-table>
+        <!-- end -->
+      </el-card>
+     </div>
+  </div>
+</template>
+
+<script>
+import {list} from '@/api/base/saasClient'
+export default {
+  name: 'saas-clients-index',
+  data () {
+    return {
+      dataList:[]
+    }
+  },
+  methods: {
+    getList() {
+      //调用API发起请求
+      //res=响应数据
+      list().then(res => {
+        this.dataList = res.data.data
+      })
+    }
+  },
+  // 创建完毕状态
+  created() {
+    this.getList()
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.alert {
+  margin: 10px 0px 0px 0px;
+}
+.pagination {
+  margin-top: 10px;
+  text-align: right;
+}
+</style>
